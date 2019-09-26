b1ae122d570c3bc3d6f03bc938ff027075266d70
IndriYuliyanti <55839650+IndriYuliyanti@users.noreply.github.com>
2019-09-26 21:11:39 +0700

    Add files via upload

---
+++ Pboulang-10115109-latihan3-nilai/src/pboulang/pkg10115109/latihan3/nilai/Pboulang10115109Latihan3Nilai.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboulang.pkg10115109.latihan3.nilai;
+import java.util.Scanner;
+/**
+ *
+ * @author User
+ */
+public class Pboulang10115109Latihan3Nilai {
+    
+    static Scanner scan = new Scanner(System.in);
+    
+    
+    
+    public static void main(String[] args) {
+        String nama;
+        
+        System.out.println(" masukan nama anda:" );
+        nama = scan.nextLine();
+        System.out.println("nama anda adalah "+ nama);
+        
+    }
+    
+}
